dce59db594497eec4fb00e2b13872444b89fc6fb
Mina Nabil Sami <mina.nsami@gmail.com>
2019-06-23 01:46:04 +0200

    create items from launches representations

--- src/Endouble/Engine/Infrastructure/Service/ItemTranslator.php
+++ src/Endouble/Engine/Infrastructure/Service/ItemTranslator.php
@@ -4,14 +4,33 @@ declare(strict_types=1);
 namespace Endouble\Engine\Infrastructure\Service;
 
 use Endouble\Engine\Domain\Model\Item\Item;
+use Endouble\Engine\Domain\Model\Item\ItemId;
+use Endouble\Engine\Domain\Model\Item\Number;
+use Endouble\Engine\Domain\Model\Item\Source;
+use Endouble\Shared\Domain\Model\Details;
+use Endouble\Shared\Domain\Model\Uri;
 
 class ItemTranslator
 {
     public function toItemsFromLaunches(array $launchesRepresentation): array
     {
+        $items = [];
+        foreach ($launchesRepresentation as $item) {
+            $items[] = new Item(
+                new ItemId(),
+                Uri::createFromString($item['links']['wikipedia']),
+                (new \DateTimeImmutable())->setTimestamp($item['launch_date_unix']),
+                $item['details'] ? new Details($item['details']) : Details::createEmpty(),
+                new Number($item['flight_number']),
+                Source::createSpacexSource()
+            );
+        }
+
+        return $items;
     }
 
     public function toItemFromLaunch(array $launchRepresentation): ?Item
     {
+        var_dump($launchRepresentation);
     }
 }
