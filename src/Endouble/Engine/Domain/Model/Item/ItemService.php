740e81d6be1351bf9a23afb2a834aea4305d028e
Mina Nabil Sami <mina.nsami@gmail.com>
2019-06-22 14:50:38 +0200

    Add ItemService

--- src/Endouble/Engine/Domain/Model/Item/ItemService.php
+++ src/Endouble/Engine/Domain/Model/Item/ItemService.php
@@ -3,17 +3,39 @@ declare(strict_types=1);
 
 namespace Endouble\Engine\Domain\Model\Item;
 
-use Endouble\Spacex\Domain\Model\Launch\Launch;
-
 interface ItemService
 {
     /**
-     * @param string $link
-     * @param \DateTimeImmutable $date
-     * @param string $details
-     * @param int $number
-     * @param string $source
+     * @param int $year
+     * @param int $limit
+     * @return array
+     */
+    public function itemsFromPastLaunches(int $year = 0, int $limit = 0): array;
+
+    /**
+     * @param int $year
+     * @param int $limit
+     * @return array
+     */
+    public function itemsFromUpcomingLaunches(int $year = 0, int $limit = 0): array;
+
+    /**
      * @return Item
      */
-    public function itemFrom(string $link, \DateTimeImmutable $date, string $details, int $number, string $source): Item;
+    public function itemFromLatestLaunch(): Item;
+
+    /**
+     * @return Item
+     */
+    public function itemFromNextLaunch(): Item;
+
+    /**
+     * @return array
+     */
+    public function itemsFromComics(): array;
+
+    /**
+     * @return Item
+     */
+    public function itemFromLatestComic(): Item;
 }
